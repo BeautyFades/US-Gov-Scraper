d9db02e74681324ee59cf0e36af87c5b12c588d3
BeautyFades <fellipedfernandes@gmail.com>
2022-12-01 13:20:27 -0300

    Extract names and jobtitles. Download csv

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from utils.logger import ScraperLogger
 import logging
 from utils.debug_functions import *
 from time import sleep
+import pandas as pd
 
 
 app = Flask('app')
@@ -19,11 +20,12 @@ logging.info('Lord forgive me for what I am about to code')
 
 
 chrome_options = uc.ChromeOptions()
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--disable-setuid-sandbox')
-chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument(
+if config.ENVIRONMENT != 'dev-local':
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-setuid-sandbox')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument(
         "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"
     )
 driver = uc.Chrome(version_main=106, options=chrome_options)
@@ -42,26 +44,39 @@ def health_check():
                 )
 
 
-
 @app.route("/api/v1/scrape", methods=["GET"])
 def scrape():
 
     logging.info(f'Scraper received a / request from {get_client_request_ip_address()}.')
  
-    driver.get('https://www.congress.gov/members')
+    driver.get('https://www.congress.gov/members?pageSize=250')
 
     elem = driver.find_element(By.XPATH, '//ol[@class="basic-search-results-lists expanded-view"]')
 
-    all_li = elem.find_elements(By.XPATH, './/li[@class="expanded"]')
+    webelements_all_li = elem.find_elements(By.XPATH, './/li[@class="expanded"]')
 
     namelist = []
-    for li in all_li:
+    job_titles = []
+    for li in webelements_all_li:
         name = li.find_element(By.XPATH, './/span/a')
-        namelist.append(name.text)
+        names = name.text.split(' - ')
+        namelist.append(names[0])
+        job_titles.append(names[1])
 
     print(namelist)
-    str1 = '\n'.join(str(e) for e in namelist)
-    return str1
+    print(job_titles)
+
+    rows = zip(namelist, job_titles)
+
+    df = pd.DataFrame(rows, columns=['name', 'job_title'])
+    df.to_csv('pd.csv')
+
+    return Response(
+        df.to_csv(),
+        mimetype="text/csv",
+        headers={"Content-disposition":
+        "attachment; filename=export.csv"}
+        )
 
 
 @app.route("/api/v1/google", methods=["GET"])
@@ -97,11 +112,10 @@ def test_session():
 
     driver.get('https://infosimples.github.io/detect-headless/')
 
-    wait3 = WebDriverWait(driver, 10).until(EC.alert_is_present())
+    WebDriverWait(driver, 10).until(EC.alert_is_present())
     sleep(2)
     driver.switch_to.alert.accept()
-
-    wait_for_element = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "/html/body/table")))
+    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "/html/body/table")))
     driver.save_screenshot('detectionResult.png')
 
     return send_file('detectionResult.png', mimetype='image/png')
